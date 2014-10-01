829c39d55c413bdc3e53c9edff13762ed6d07194
kullua <b@loveu.tw>
2014-10-01 16:56:26 +0800

    icon

--- gen/tw/rx/helperguides/R.java
+++ gen/tw/rx/helperguides/R.java
@@ -1077,7 +1077,8 @@ containing a value of this type.
         public static final int ic_guides2=0x7f02005a;
         public static final int ic_guides3=0x7f02005b;
         public static final int ic_launcher=0x7f02005c;
-        public static final int top=0x7f02005d;
+        public static final int icon_logo=0x7f02005d;
+        public static final int top=0x7f02005e;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1096,32 +1097,32 @@ containing a value of this type.
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int content_frame=0x7f050052;
+        public static final int content_frame=0x7f050053;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
-        public static final int drawer_layout=0x7f050051;
+        public static final int drawer_layout=0x7f050052;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
-        public static final int errortext=0x7f050050;
+        public static final int errortext=0x7f050051;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
-        public static final int id=0x7f05004e;
+        public static final int id=0x7f05004f;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int image1=0x7f050043;
-        public static final int image2=0x7f050044;
-        public static final int image3=0x7f050049;
-        public static final int image4=0x7f05004a;
+        public static final int image1=0x7f050044;
+        public static final int image2=0x7f050045;
+        public static final int image3=0x7f05004a;
+        public static final int image4=0x7f05004b;
         public static final int imageBanner=0x7f05003d;
-        public static final int left_drawer=0x7f050053;
+        public static final int left_drawer=0x7f050054;
         public static final int line=0x7f05003f;
         public static final int listMode=0x7f050001;
-        public static final int listView1=0x7f05004d;
+        public static final int listView1=0x7f05004e;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1146,25 +1147,26 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int space=0x7f05003e;
         public static final int split_action_bar=0x7f05001e;
-        public static final int subject=0x7f05004f;
+        public static final int subject=0x7f050050;
         public static final int submit_area=0x7f050039;
-        public static final int sv_search=0x7f050040;
+        public static final int sv_search=0x7f050041;
         public static final int tabMode=0x7f050002;
-        public static final int tableLayout1=0x7f050041;
-        public static final int tableRow1=0x7f050042;
-        public static final int tableRow2=0x7f050045;
+        public static final int tableLayout1=0x7f050042;
+        public static final int tableRow1=0x7f050043;
+        public static final int tableRow2=0x7f050046;
         public static final int textView1=0x7f05003c;
-        public static final int textView2=0x7f050046;
-        public static final int textView3=0x7f050047;
-        public static final int textView4=0x7f050048;
-        public static final int textView5=0x7f05004b;
-        public static final int textView6=0x7f05004c;
+        public static final int textView2=0x7f050047;
+        public static final int textView3=0x7f050048;
+        public static final int textView4=0x7f050049;
+        public static final int textView5=0x7f05004c;
+        public static final int textView6=0x7f05004d;
+        public static final int textView7=0x7f050040;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int typename=0x7f050054;
+        public static final int typename=0x7f050055;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int webview=0x7f050055;
+        public static final int webview=0x7f050056;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1285,6 +1287,7 @@ containing a value of this type.
         public static final int hello_world=0x7f0a0015;
         public static final int mainTitle=0x7f0a000e;
         public static final int searchTitle=0x7f0a0014;
+        public static final int searchtext=0x7f0a001a;
     }
     public static final class style {
         /** 
